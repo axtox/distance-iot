db86245c7152ba9fb30c707b35ff1f421db98b43
axtox <axtoox@protonmail.com>
2022-05-28 04:48:04 +0300

    Deployment settings edited Now the project will be compiled as one executable file The send-to-pi.sh script wont promnt for password anymore

--- send-to-pi.sh
+++ send-to-pi.sh
@@ -1,4 +1,4 @@
-dotnet publish -r linux-arm
-pushd ./bin/Debug/net6.0/linux-arm/publish
-scp -r . pi:/home/hud/Projects/Distance
+dotnet publish -r linux-arm -c Release --self-contained true
+pushd ./bin/Release/net6.0/linux-arm/publish
+sshpass -p 6464 scp -r . pi:/home/hud/Projects/Distance
 popd
