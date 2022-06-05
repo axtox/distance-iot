abfa54cba987f29d64c8ef3f34f3612b2f02fd0a
axtox <axtoox@protonmail.com>
2022-06-05 03:36:48 +0300

    Device base class is now IDisposable

--- Device.cs
+++ Device.cs
@@ -1,12 +1,35 @@
 using System.Device.Gpio;
 
 namespace Distance {
-    internal abstract class Device {
+    internal abstract class Device : IDisposable {
+        private bool _disposedValue;
+
         protected GpioController _controller;
 
         public Device(GpioController controller)
         {
             _controller = controller;
+        }
+
+        ~Device() => Dispose(false);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _controller.Dispose();
+                }
+
+                _disposedValue = true;
+            }
         }
     }
 }
