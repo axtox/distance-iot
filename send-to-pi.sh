b25e497d7c1182727c1bc6575c5de94728bb8f4f
axtox <axtoox@protonmail.com>
2022-06-10 03:40:03 +0300

    Publish script edited to be used with debugger

--- send-to-pi.sh
+++ send-to-pi.sh
@@ -1,4 +1,4 @@
-dotnet publish -r linux-arm -c Release --self-contained true
-pushd ./bin/Release/net6.0/linux-arm/publish
+dotnet publish -r linux-arm -c Debug 
+pushd ./bin/Debug/net6.0/linux-arm/publish
 sshpass -p 6464 scp -r . pi:/home/hud/Projects/Distance
 popd
